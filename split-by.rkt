2696668fecd9cca27f19df00873aac9654593ede
Jack Firth <jackhfirth@gmail.com>
2015-09-23 21:11:42 -0700

    Depend on racket/base, add empty test case

--- split-by.rkt
+++ split-by.rkt
@@ -1,18 +1,30 @@
-#lang racket
+#lang racket/base
+
 (provide split-by)
+
+(require racket/list)
+
 (define (split-by l f)
   (if (null? l)
       null
-      (let loop ([groups null] [acc (list (car l))] [l (cdr l)] [val (f (car l))])
+      (let loop ([groups null] [acc (list (first l))] [l (rest l)] [val (f (first l))])
         (cond
           [(null? l) (reverse (cons acc groups))]
           [else
-           (define c (car l))
+           (define c (first l))
            (define v (f c))
            (if (equal? val v)
-               (loop groups (cons c acc) (cdr l) val)
-               (loop (cons (reverse acc) groups) (list c) (cdr l) v))]))))
+               (loop groups (cons c acc) (rest l) val)
+               (loop (cons (reverse acc) groups) (list c) (rest l) v))]))))
+
 (module+ test
   (require rackunit)
-  (check-equal? (split-by (range 1.0 5.1 .5) floor)
-                '((1.0 1.5) (2.0 2.5) (3.0 3.5) (4.0 4.5) (5.0))))
+
+  (define (should-never-be-called)
+    (error "was called, shouldn't have been"))
+
+  (check-equal? (split-by '() should-never-be-called)
+                '())
+
+  (check-equal? (split-by '(1.0 1.5 2.0 2.5 3.0) floor)
+                '((1.0 1.5) (2.0 2.5) (3.0))))
